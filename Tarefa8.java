10ecaaa857c57247fe5df527a7d29f9830f4db13
081Jffrson <jeffersonmarianodasilva6@gmail.com>
2025-10-25 10:45:00 -0300

    Adicionando arquivo PDF

---
+++ Tarefa8.java
@@ -0,0 +1,18 @@
+
+/**
+ * Tarefa13
+ */
+public class Tarefa8 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100 ; i += 2 ){
+        System.out.println(i);
+    }
+
+
+
+
+
+    }
+
+    
+}
